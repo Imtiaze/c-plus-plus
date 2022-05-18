d0a6569e38bb35a1f72b7143ce7d7f54cab9c44e
Imtiaze <ahammedimtiaze78@gmail.com>
2022-05-19 00:26:48 +0600

    Awesome patterns

---
+++ 4.1 - Awesome Patterns/005.cpp
@@ -0,0 +1,33 @@
+/* 
+
+print : Half pyramid after 180 degree rotation
+
+
+    *
+   **
+  ***
+ ****
+*****
+
+ */
+
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    for (int i = n; i >= 1; i--) {
+        for (int j = 1; j <= n ; j++) {
+            if (i <= j) {
+                cout << "*";
+            } else {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+}
